f8a9e26879189536071c1e144dd6bd192cb35482
lichao890427 <lichao.890427@163.com>
2017-03-18 00:08:44 +0800

    first push

---
+++ DexDump/app/src/main/obj/local/armeabi/objs-debug/test.cy/test.o.d
@@ -0,0 +1,191 @@
+E:/DexDump/app/src/main/obj/local/armeabi/objs-debug/test.cy/test.o: \
+ E:/DexDump/app/src/main/jni/test.cpp \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/jni.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/unistd.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/stdint.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/signal.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/limits.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/string.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/malloc.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/ucontext.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/user.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm/sigcontext.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/pathconf.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/stdio.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/fcntl.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/fcntl.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm/fcntl.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/android/log.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/stat.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/stat.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/endian.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/endian.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/machine/endian.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/errno.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/errno.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm/errno.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h \
+ E:/DexDump/app/src/main/jni/dexfile.h \
+ E:/DexDump/app/src/main/jni/substrate.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/dlfcn.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/alloca.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/strings.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/memory.h
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/jni.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/signal.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/limits.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/string.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/ucontext.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/user.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm/sigcontext.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/pathconf.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/fcntl.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/fcntl.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm/fcntl.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/android/log.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/stat.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/stat.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/endian.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/sys/endian.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/machine/endian.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/errno.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/linux/errno.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm/errno.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h:
+
+E:/DexDump/app/src/main/jni/dexfile.h:
+
+E:/DexDump/app/src/main/jni/substrate.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/dlfcn.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/strings.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-3/arch-arm/usr/include/memory.h:
---
+++ DexDump/app/src/main/obj/local/x86/objs-debug/test.cy/test.o.d
@@ -0,0 +1,197 @@
+E:/DexDump/app/src/main/obj/local/x86/objs-debug/test.cy/test.o: \
+ E:/DexDump/app/src/main/jni/test.cpp \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/jni.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/cdefs.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/android/api-level.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/unistd.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/types.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/stdint.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/_types.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/machine/_types.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/posix_types.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/stddef.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/compiler.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/posix_types.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/types.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/types.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/machine/kernel.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/select.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/time.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/time.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/signal.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/limits.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/limits.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/limits.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/machine/internal_types.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/machine/limits.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/syslimits.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/page.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/page_32.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/string.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/malloc.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/signal.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm-generic/signal.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/sigcontext.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/siginfo.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm-generic/siginfo.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/ucontext.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/user.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/sysconf.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/capability.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/pathconf.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/stdio.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/fcntl.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/fcntl.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/fcntl.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm-generic/fcntl.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/android/log.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/stat.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/stat.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/endian.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/endian.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/machine/endian.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/errno.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/errno.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/errno.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm-generic/errno.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm-generic/errno-base.h \
+ E:/DexDump/app/src/main/jni/dexfile.h \
+ E:/DexDump/app/src/main/jni/substrate.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/dlfcn.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/stdlib.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/alloca.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/strings.h \
+ E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/memory.h
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/jni.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/cdefs.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/android/api-level.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/unistd.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/types.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/stdint.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/_types.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/machine/_types.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/posix_types.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/stddef.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/compiler.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/posix_types.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/types.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/types.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/machine/kernel.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/select.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/time.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/time.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/signal.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/limits.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/limits.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/limits.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/machine/internal_types.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/machine/limits.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/syslimits.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/page.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/page_32.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/string.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/malloc.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/signal.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm-generic/signal.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/sigcontext.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/siginfo.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm-generic/siginfo.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/ucontext.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/user.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/sysconf.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/capability.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/pathconf.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/stdio.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/fcntl.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/fcntl.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/fcntl.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm-generic/fcntl.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/android/log.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/stat.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/stat.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/endian.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/sys/endian.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/machine/endian.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/errno.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/linux/errno.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm/errno.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm-generic/errno.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/asm-generic/errno-base.h:
+
+E:/DexDump/app/src/main/jni/dexfile.h:
+
+E:/DexDump/app/src/main/jni/substrate.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/dlfcn.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/stdlib.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/alloca.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/strings.h:
+
+E:/Android/ndk/android-ndk-r12b/platforms/android-9/arch-x86/usr/include/memory.h:
